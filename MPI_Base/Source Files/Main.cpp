f9e8a06a6168ed4981f1e0c0c4e7f01bf99d3d9b
Nick Pickering <npick001@odu.edu>
2023-11-29 17:13:09 -0500

    capturing timing data

--- MPI_Base/Source Files/Main.cpp
+++ MPI_Base/Source Files/Main.cpp
@@ -1,3 +1,5 @@
+#include <chrono>
+
 #include "Airport.h"
 #include "SimulationExecutive.h"
 
@@ -9,6 +11,7 @@ int main()
 	// takes a while to run when writing all data to files,
 	// I recommend checking the directives before running.
 	// command arg: -localonly -n 2 "$(TargetPath)"
+	auto start_time = std::chrono::high_resolution_clock::now();
 
 	CommunicationInitialize();
 
@@ -24,17 +27,23 @@ int main()
 	InitializeSimulation();
 
 	// make sure all are started
-	std::cout << "Process " << rank << " done with initialization." << std::endl;
+	//std::cout << "Process " << rank << " done with initialization." << std::endl;
 	Barrier();
-	std::cout << "Process " << rank << " allowed through barrier." << std::endl;
+	//std::cout << "Process " << rank << " allowed through barrier." << std::endl;
 
 	RunSimulation(100);
 
 	// I want to implement the termination messages, but the barrier works so well
-	std::cout << "Process " << rank << " done with simulation." << std::endl;
+	//std::cout << "Process " << rank << " done with simulation." << std::endl;
 	Barrier();
-	std::cout << "Process " << rank << " allowed through barrier." << std::endl;
+	//std::cout << "Process " << rank << " allowed through barrier." << std::endl;
 
 	CommunicationFinalize();
+
+	auto end_time = std::chrono::high_resolution_clock::now();
+	double clock_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+	clock_time /= 1000.0;
+
+	std::cout << "Process " << rank << " program execution time: " << clock_time << "s" << std::endl;
 	return 0;
 }
